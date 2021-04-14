f517a0ac296d045a465f7cf8d5c6fa1f042ae934
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-04-14 18:38:18 +0930

    refactor: Abstract away db methods to dedicated service

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,16 @@
 const router = require("express").Router();
-const fs = require("fs");
+
+// Import input validation middleware
 const validate = require("../utils/validator");
+
+// Import notes service
+const notesService = require("../utils/notesService");
 
 // Get all notes
 router.get("/notes", async (req, res) => {
   try {
-    // Read raw data from file
-    const data = fs.readFileSync("./db/db.json", "utf8", (err, data) => {
-      if (err) {
-        throw err;
-      }
-      return data;
-    });
-
     // Parse data to JSON
-    const notes = JSON.parse(data);
+    const notes = notesService.readNotes();
 
     // Respond with parsed JSON data
     res.json(notes);
@@ -26,33 +22,12 @@ router.get("/notes", async (req, res) => {
 // Save new note
 router.post("/notes", validate.validateNote, async (req, res) => {
   try {
-    // Read raw data from file
-    const data = fs.readFileSync("./db/db.json", "utf8", (err, data) => {
-      if (err) {
-        throw err;
-      }
-      return data;
-    });
+    notesService.createNote(req.body);
 
-    // Parse data to JSON
-    const notes = JSON.parse(data);
-
-    // Retrieve and store posted note data from request body
-    const note = req.body;
-
-    // Push new note to notes array
-    notes.push(note);
-
-    // Write updated notes data to file
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes), (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-
+    // Force redirect to reflect changes in view
     res.redirect("/notes");
   } catch (err) {
-    console.log("error block");
+    throw err;
   }
 });
 
