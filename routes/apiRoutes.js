eaf94bfc95b8b041bb48568ce5472e44da8fab37
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-04-14 17:10:39 +0930

    feat: Implement save note functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const fs = require("fs");
+const validate = require("../utils/validator");
 
+// Get all notes
 router.get("/notes", async (req, res) => {
   try {
     // Read raw data from file
@@ -21,6 +23,37 @@ router.get("/notes", async (req, res) => {
   }
 });
 
-router.post("/notes", async (req, res) => {});
+// Save new note
+router.post("/notes", validate.validateNote, async (req, res) => {
+  try {
+    // Read raw data from file
+    const data = fs.readFileSync("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      return data;
+    });
+
+    // Parse data to JSON
+    const notes = JSON.parse(data);
+
+    // Retrieve and store posted note data from request body
+    const note = req.body;
+
+    // Push new note to notes array
+    notes.push(note);
+
+    // Write updated notes data to file
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+
+    res.redirect("/notes");
+  } catch (err) {
+    console.log("error block");
+  }
+});
 
 module.exports = router;
